1478573242aa596614f4a8dbb55de900a071c362
Lars Johnson <keepitlj@gmail.com>
2015-11-22 16:46:10 -0600

    Update Bernies Start From Scratch

---
+++ week-2/bernies-start-from-scratch/bistro-1.rb
@@ -0,0 +1,62 @@
+require 'csv'
+
+class Recipe
+  attr_reader :id, :name, :description, :ingredients, :directions
+
+  def initialize(args = {})
+    @id = args["id"]
+    @name = args["name"]
+    @description = args["description"]
+    @ingredients = args["ingredients"]
+    @directions = args["directions"]
+  end
+
+  def to_s
+    "\nRecipe #{id} – #{name}\n#{description}\n#{ingredients}\n#{directions}\n"
+  end
+
+end
+
+class Bistro
+  attr_reader :recipes, :recipe
+
+  def initialize(filename)
+    @recipes = []
+    load_recipes(filename)
+  end
+
+  def load_recipes(filename)
+    CSV.foreach(filename, headers: true) do |row|
+      add_recipe(Recipe.new(row))
+    end
+  end
+
+  def list
+    recipes.each_with_index do |recipe, index|
+    puts "#{index+1}. #{recipe.name}"
+    end
+  end
+
+  def add_recipe(recipe)
+    @recipes << recipe
+  end
+
+  def find_recipe_by_id(recipe_id)
+    recipes.each do |recipe|
+      if recipe.id == recipe_id.to_s
+        return recipe
+      end
+    end
+    raise "The recipe you are looking for does not exist"
+  end
+end
+
+bistro = Bistro.new("recipes.csv")
+bistro.list
+puts bistro.find_recipe_by_id(938)
+
+if ARGV[0] == "list"
+  bistro.list
+elsif ARGV[0] == "display"
+  puts bistro.find_recipe_by_id(ARGV[1])
+end
